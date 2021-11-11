af131812de2114c8d2b5e18e11f1ea75f4c50007
Nestor Leandro Villalobos Carrillo <leandrovillaloboscarrillogmail.com>
2021-11-11 07:48:30 -0500

    [FIX] se modificas el axios

--- frontend/src/components/Product/QualificationBar.jsx
+++ frontend/src/components/Product/QualificationBar.jsx
@@ -13,24 +13,6 @@ export default function QualificationBar({id}) {
 
     let star = (i) => <svg key={i} index={i} viewBox="0 0 15 17" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.27778 0L8.91174 4.83688H14.1994L9.92159 7.82624L11.5555 12.6631L7.27778 9.67376L3.00001 12.6631L4.63397 7.82624L0.3562 4.83688H5.64382L7.27778 0Z" className={i >= starIndex ? Styles.star : Styles.starActive} /></svg>;
 
-    useEffect(() => {
-        axios
-            .post("http://localhost:8080/products/scores/create", {                
-                score: starIndex,
-                userEmail: sessionStorage.getItem('email'),                
-                productId:id                
-            })
-            .then((response) => {
-                if (response.status == 200) {
-                    setCalificacion_text(`Se envió correctamente la puntuación de: ${starIndex}`);
-                }               
-
-            })
-            .catch((error) => {
-                setErrorMessage(error);
-            });
-    }, [submit]);
-
     console.log(sessionStorage.getItem('email'))
 
     const renderStar = () => {
@@ -52,6 +34,22 @@ export default function QualificationBar({id}) {
         setSubmit(true);
         sessionStorage.setItem('calificacion', starIndex);
         setCalificacion_text(`Enviando puntuación...`);
+        axios
+            .post("http://localhost:8080/products/scores/create", {                
+                score: starIndex,
+                userEmail: sessionStorage.getItem('email'),                
+                productId:id                
+            })
+            .then((response) => {
+                if (response.status == 200) {
+                    setCalificacion_text(`Se envió correctamente la puntuación de: ${starIndex}`);
+                }               
+
+            })
+            .catch((error) => {
+                setErrorMessage(error);
+            });
+
     }
 
     const handleReset = () => {
