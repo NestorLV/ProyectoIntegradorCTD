faaa4cec39676983177e25ebe45907cb42f52e2f
strickerkarin <strickerkarin@hotmail.com>
2021-11-11 10:53:30 -0300

    Responsive qualification

--- frontend/src/components/Product/QualificationBar.jsx
+++ frontend/src/components/Product/QualificationBar.jsx
@@ -1,12 +1,12 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import StylesApp from "../../App.module.css";
 import Styles from "./styles.module.css";
 import axios from "axios";
 
-export default function QualificationBar({id}) {
-    const [starIndex, setStarIndex] = useState(sessionStorage.getItem('calificacion')==null?0:sessionStorage.getItem('calificacion'));
+export default function QualificationBar({ id }) {
+    const [starIndex, setStarIndex] = useState(sessionStorage.getItem('calificacion') == null ? 0 : sessionStorage.getItem('calificacion'));
     const [submit, setSubmit] = useState(false);
-    const [calificacion_text, setCalificacion_text] = useState(`Puntuación: ${starIndex===0?"":starIndex}`);
+    const [calificacion_text, setCalificacion_text] = useState(`Puntuación: ${starIndex === 0 ? "" : starIndex}`);
     const [errorMessage, setErrorMessage] = useState("");
 
     let stars = [];
@@ -35,15 +35,15 @@ export default function QualificationBar({id}) {
         sessionStorage.setItem('calificacion', starIndex.toString());
         setCalificacion_text(`Enviando puntuación...`);
         axios
-            .post("http://localhost:8080/products/scores/create", {                
+            .post("http://localhost:8080/products/scores/create", {
                 score: starIndex,
-                userEmail: sessionStorage.getItem('email'),                
-                productId:id                
+                userEmail: sessionStorage.getItem('email'),
+                productId: id
             })
             .then((response) => {
                 if (response.status === 200) {
                     setCalificacion_text(`Se envió correctamente la puntuación de: ${starIndex}`);
-                }               
+                }
 
             })
             .catch((error) => {
@@ -61,19 +61,21 @@ export default function QualificationBar({id}) {
     return (
         <div className={`${Styles.qualificationBar} ${StylesApp.delimiter}`}>
             <div className={`${Styles.qualificationBarChild} ${StylesApp.delimiterChild}`}>
-                <h2>Califica este alojamiento: </h2>
-                <div className={Styles.starsMap}>
-                    {stars.map((star, i) => {
-                        return <div key={i} className={Styles.starBox} onClick={() => handleStarIndex(i + 1)} >{star}</div>;
-                    })}
+                <div className={Styles.qualificationBarTitle}>
+                    <h2>Califica este alojamiento: </h2>
+                    <div className={Styles.starsMap}>
+                        {stars.map((star, i) => {
+                            return <div key={i} className={Styles.starBox} onClick={() => handleStarIndex(i + 1)} >{star}</div>;
+                        })}
+                    </div>
                 </div>
-                <div className={Styles.qualificationButtons}>
-                    <button className={Styles.qualificationReset} onClick={handleReset} > Resetear </button>
-                    <button className={Styles.qualificationSubmit} onClick={starIndex > 0 ? handleSubmit : null} > Enviar     </button>
-                    <div className={Styles.qualificationConfirm} >{calificacion_text}</div>
-                </div>
+                    <div className={Styles.qualificationButtons}>
+                        <button className={Styles.qualificationReset} onClick={handleReset} > Resetear </button>
+                        <button className={Styles.qualificationSubmit} onClick={starIndex > 0 ? handleSubmit : null} > Enviar     </button>
+                        <div className={Styles.qualificationConfirm} >{calificacion_text}</div>
+                    </div>
 
+                </div>
             </div>
-        </div>
-    )
+            )
 }
