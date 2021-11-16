f790d2ced00eda43e51c61bffe5bf6bc2bdf6959
Mariel Romeo <marielceciliaromeo@hotmail.com>
2021-11-16 17:51:12 -0300

    [ADD] estructura carpeta booking

---
+++ frontend/src/components/booking/Booking.jsx
@@ -0,0 +1,10 @@
+import FormBooking from "./FormBooking"
+import Styles from "./styles.module.css"
+
+export default function Booking(){
+    return(
+        <section>
+            <FormBooking/>
+        </section>
+    )
+}
---
+++ frontend/src/components/booking/FormBooking.jsx
@@ -0,0 +1,7 @@
+import Styles from "./styles.module.css"
+
+export default function FormBooking(){
+    return(
+
+    )
+}
