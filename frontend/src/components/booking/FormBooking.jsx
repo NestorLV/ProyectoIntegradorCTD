132dc00fb2028a83761a3233f8d6552f634b0304
Mariel Romeo <marielceciliaromeo@hotmail.com>
2021-11-16 18:14:00 -0300

    [FIX] disableDates

--- frontend/src/components/booking/FormBooking.jsx
+++ frontend/src/components/booking/FormBooking.jsx
@@ -2,6 +2,12 @@ import Styles from "./styles.module.css"
 
 export default function FormBooking(){
     return(
-
+        <>
+            <h3>Completá tus datos</h3>
+            <form>
+                <label>Nombre</label>
+                <input></input>
+            </form>
+        </>
     )
 }
