d2f413756053822c276121910ae41c22fdaef0e2
Mariel Romeo <marielceciliaromeo@hotmail.com>
2021-11-17 11:42:42 -0300

    [ADD]css al formBooking

--- frontend/src/components/booking/Booking.jsx
+++ frontend/src/components/booking/Booking.jsx
@@ -1,10 +1,18 @@
 import FormBooking from "./FormBooking"
 import Styles from "./styles.module.css"
+import StylesApp from "../../App.module.css"
 
-export default function Booking(){
-    return(
-        <section>
-            <FormBooking/>
+export default function Booking() {
+    return (
+        <section className={`${Styles.containerPrincipal} ${StylesApp.delimiter}`}>
+            <div className={`${StylesApp.delimiterChild}`}>
+                <div className={`${Styles.barraIzq}`}>
+                    <FormBooking />
+                </div>
+                <div className={Styles.barraDer}>
+
+                </div>
+            </div>
         </section>
     )
 }
--- frontend/src/components/booking/FormBooking.jsx
+++ frontend/src/components/booking/FormBooking.jsx
@@ -1,27 +1,34 @@
 import Styles from "./styles.module.css"
 
-export default function FormBooking(){
-    return(
-        <>
+import StylesApp from "../../App.module.css"
+
+export default function FormBooking() {
+    return (
+        <div className={`${Styles.containerForm}`}>
             <h3>Completá tus datos</h3>
             <form>
-                <div>
-                    <label>Nombre</label>
-                    <input type="text" name="name" id="name" disabled></input>
+                <div className={Styles.block}>
+                    <div className={Styles.labelInput}>
+                        <label>Nombre</label>
+                        <input type="text" name="name" id="name" disabled></input>
+                    </div>
+                    <div className={Styles.labelInput}>
+                        <label>Apellido</label>
+                        <input type="text" name="surname" id="surname" disabled></input>
+                    </div>
                 </div>
-                <div>
-                    <label>Apellido</label>
-                    <input type="text" name="surname" id="surname" disabled></input>
-                </div>
-                <div>
-                <label for="email">Correo electrónico</label>
-                    <input type="email" name="email" id="email" disabled/>
-                </div>
-                <div>
-                    <label>Ciudad</label>
-                    <input type="text" name="surname" id="surname" required></input>
+                <div className={Styles.block}>
+                    <div className={Styles.labelInput}>
+                        <label for="email">Correo electrónico</label>
+                        <input type="email" name="email" id="email" disabled />
+                    </div>
+                    <div className={Styles.labelInput}>
+                        <label>Ciudad</label>
+                        <input type="text" name="surname" id="surname" required></input>
+                    </div>
                 </div>
             </form>
-        </>
+
+        </div>
     )
 }
