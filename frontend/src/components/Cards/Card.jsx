409ef60b18d719eb21797e4cf80333e74f2ddc5c
strickerkarin <strickerkarin@hotmail.com>
2021-11-17 17:22:43 -0300

    Modifico estilos icono heart Cards

--- frontend/src/components/Cards/Card.jsx
+++ frontend/src/components/Cards/Card.jsx
@@ -72,7 +72,7 @@ function Card({ image, cardCategory, name, city, country, description, id, refer
 
         <div className={Styles.cardBox}>
             <div className={Styles.cardImage}>
-                <svg className={Styles.iconHeart} onClick={loggued === "true" ? handleToggle : openModalFavourite} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path className={isLike ? Styles.heartColor2 : Styles.heartColor} id="heart" d="M12 4.248c-3.148-5.402-12-3.825-12 2.944 0 4.661 5.571 9.427 12 15.808 6.43-6.381 12-11.147 12-15.808 0-6.792-8.875-8.306-12-2.944z" /></svg>
+                <svg className={Styles.iconHeart} onClick={loggued === "true" ? handleToggle : openModalFavourite} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 27 27"><path className={isLike ? Styles.heartColor2 : Styles.heartColor} id="heart" d="M12 4.248c-3.148-5.402-12-3.825-12 2.944 0 4.661 5.571 9.427 12 15.808 6.43-6.381 12-11.147 12-15.808 0-6.792-8.875-8.306-12-2.944z" /></svg>
                 <Modal open={modalFavouriteIsOpen} onClose={closeModalFavourite} center>
                     <div className={Styles.modalFavourite}>
                         <p>Para agregar favoritos, ingresa a tu cuenta</p>
