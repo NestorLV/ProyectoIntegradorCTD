55efaff4aa83246f165acfaffb8bd25bf7f4669a
Nestor Leandro Villalobos Carrillo <leandrovillaloboscarrillogmail.com>
2021-11-20 15:54:54 -0500

    [FIX] se cambia las estrellas de detailBar de booking por el componente

--- frontend/src/components/booking/DetailBar.jsx
+++ frontend/src/components/booking/DetailBar.jsx
@@ -1,12 +1,12 @@
 import Styles from "./styles.module.css"
 import iconLocation from "../Cards/img/IconLocation.svg";
-import iconStar from "../Cards/img/starOrange.png";
+import ScoreStar from "../Product/ScoreStar";
 
 function DetailBar(props) {
-    const { image, category, name, city, country, reference, qualification, checkin, checkout} = props;
+    const { image, category, name, city, country, reference, qualification, checkin, checkout } = props;
 
     let cantStar = Math.floor(qualification / 2);
-  
+
     return (
         <div className={Styles.detailBar}>
             <h2>Detalle de la Reserva</h2>
@@ -16,13 +16,7 @@ function DetailBar(props) {
                     <div className={Styles.detailProduct}>
                         <p>{category}</p>
                         <h3>{name}</h3>
-                        <div className={Styles.startContainer}>
-                            {cantStar >= 0 ? <img className={Styles.star} src={iconStar} alt="" /> : null}
-                            {cantStar >= 2 ? <img className={Styles.star} src={iconStar} alt="" /> : null}
-                            {cantStar >= 3 ? <img className={Styles.star} src={iconStar} alt="" /> : null}
-                            {cantStar >= 4 ? <img className={Styles.star} src={iconStar} alt="" /> : null}
-                            {cantStar >= 5 ? <img className={Styles.star} src={iconStar} alt="" /> : null}
-                        </div>
+                        <ScoreStar qualification={qualification} starColor="var(--primary-color)" />
                         <p>
                             <img className={Styles.iconLocation} src={iconLocation} alt="" />
                             {city},&#160;{country},&#160;{reference}
