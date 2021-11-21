5d8d76718a6a72ceda63b683bb44621179eacfe8
Mariel Romeo <marielceciliaromeo@hotmail.com>
2021-11-20 23:42:24 -0300

    [ADD] endpoint getByEmail y modifico endpoint de login para que devuelva el nombre y apellido del usuario junto con el token

--- backend/src/main/resources/scriptPI.sql
+++ backend/src/main/resources/scriptPI.sql
@@ -474,4 +474,11 @@ VALUES (8,4);
 INSERT INTO `proyecto_integrador`.`featurePerProduct` (idFeature,idProduct)
 VALUES (11,11);
 
-INSERT INTO `proyecto_integrador`.`user` (`idUser`, `userEmail`, `userName`) VALUES ('1', 'grupo1@digital.com', 'grupo1');
+INSERT INTO `proyecto_integrador`.`role`(name)
+VALUES (1);
+
+INSERT INTO `proyecto_integrador`.`user` (`userName`, `userSurname`,`userEmail`, `password`) VALUES ('grupo','uno', 'grupo1@digital.com', 'grupouno');
+
+SELECT * FROM user;
+
+DELETE FROM user WHERE id=3
