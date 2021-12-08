a03d33c62c5e187167ed922afe5f2e3c4985fd34
strickerkarin <strickerkarin@hotmail.com>
2021-12-08 17:01:44 -0300

    [FIX] posición paginación de Mis reservas

--- frontend/src/components/Product/QualificationBar.jsx
+++ frontend/src/components/Product/QualificationBar.jsx
@@ -14,12 +14,10 @@ export default function QualificationBar({ id }) {
     let star = (i) => <svg key={i} index={i} viewBox="0 0 15 17" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.27778 0L8.91174 4.83688H14.1994L9.92159 7.82624L11.5555 12.6631L7.27778 9.67376L3.00001 12.6631L4.63397 7.82624L0.3562 4.83688H5.64382L7.27778 0Z" className={i >= starIndex ? Styles.star : Styles.starActive} /></svg>;
     
     useEffect(()=>{
-        AxiosGetProductScore(sessionStorage.getItem("email"), id, setStarIndex);
-        console.log(starIndex,"starIndex useEffect");
+        AxiosGetProductScore(sessionStorage.getItem("email"), id, setStarIndex);       
         setCalificacion_text(`Seleccione puntuación`);
 
-    },[])
-    
+    },[])    
 
     const renderStar = () => {
         for (let i = 0; i < 5; i++) {
