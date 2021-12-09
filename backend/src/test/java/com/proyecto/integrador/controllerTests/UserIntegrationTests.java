9ea31a6f07079dd886c08f93344f436dbb295212
tatianarincon96 <tatianalunarincon@gmail.com>
2021-12-08 22:25:39 -0300

    [ADD] integration tests

--- backend/src/test/java/com/proyecto/integrador/controllerTests/UserIntegrationTests.java
+++ backend/src/test/java/com/proyecto/integrador/controllerTests/UserIntegrationTests.java
@@ -4,15 +4,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.proyecto.integrador.DTO.*;
-import com.proyecto.integrador.controller.ReservationController;
+import com.proyecto.integrador.controller.UserController;
 import com.proyecto.integrador.exceptions.BadRequestException;
 import com.proyecto.integrador.exceptions.FindByIdException;
 import com.proyecto.integrador.persistence.entity.enums.RolesTypes;
 import com.proyecto.integrador.service.impl.CategoryServiceImpl;
 import com.proyecto.integrador.service.impl.CityServiceImpl;
-import com.proyecto.integrador.service.impl.ProductServiceImpl;
-import com.proyecto.integrador.service.impl.UserServiceImpl;
-import com.proyecto.integrador.util.DatesConverter;
+import com.proyecto.integrador.service.impl.RoleServiceImpl;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -29,41 +27,50 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.time.LocalDate;
-import java.util.Date;
-
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc(addFilters = false)
 @SpringBootTest
 @AutoConfigureTestDatabase
-public class ReservationIntegrationTests {
+public class UserIntegrationTests {
 
     @Autowired
     private MockMvc mvc;
     @Autowired
-    private ReservationController reservationController;
+    private UserController userController;
     @Autowired
-    private UserServiceImpl userService;
-    @Autowired
-    private ProductServiceImpl productService;
-    @Autowired
-    private CategoryServiceImpl categoryService;
-    @Autowired
-    private CityServiceImpl cityService;
+    private RoleServiceImpl roleService;
 
     public void cargarDatos() throws FindByIdException, BadRequestException {
-        CategoryDTO categoryDTO = categoryService.save(new CategoryDTO(1,"Apart Hotels", "500.012", "http://exampleproduct.com"));
-        CityDTO cityDTO = cityService.save(new CityDTO (1,"Merlo","Argentina"));
-        ProductDTO product = productService.save(new ProductDTO("Hotel Lolandia","Hotel 5 estrellas",142.45,520.67,"Calle Falsa 123, CABA",5,"En el centro",categoryDTO,cityDTO,"rules","health","politics"));
-        UserResponseDTO user = userService.save(new UserRequestDTO("Lalo","Landa","tatyluna74@gmail.com","1234", new RoleDTO(RolesTypes.USER)));
-        reservationController.create(new ReservationDTO("10:30",DatesConverter.convertLocalDateToSqlDate(LocalDate.of(2021,10,03)), DatesConverter.convertLocalDateToSqlDate(LocalDate.of(2021,10,13)),product.getId(),user.getId()));
-        reservationController.create(new ReservationDTO("11:30",DatesConverter.convertLocalDateToSqlDate(LocalDate.of(2021,10,13)), DatesConverter.convertLocalDateToSqlDate(LocalDate.of(2021,10,23)),product.getId(),user.getId()));
+        RoleDTO role = roleService.save(new RoleDTO(RolesTypes.USER));
+        userController.create(new UserRequestDTO("Lalo","Landa","tatyluna74@gmail.com","1234",role));
+        userController.create(new UserRequestDTO("Lalo2","Landa2","tatianalunarincon@gmail.com","1234", role));
     }
 
     @Test
-    public void buscarTodasLasReservas() throws Exception {
-        this.cargarDatos();
-        MvcResult response = mvc.perform(MockMvcRequestBuilders.get("/reservations/all")
+    public void crearUser() throws Exception {
+        UserRequestDTO payloadDTO = new UserRequestDTO("Lalo2","Landa2","tatianalunarincon@hotmail.com","1234", roleService.findByName(RolesTypes.USER));
+        UserResponseDTO responseDTO = new UserResponseDTO(3,"Lalo2","Landa2","tatianalunarincon@hotmail.com",false, RolesTypes.USER);
+
+        ObjectWriter writer = new ObjectMapper()
+                .configure(SerializationFeature.WRAP_ROOT_VALUE, false)
+                .writer();
+
+        String userEntity = writer.writeValueAsString(payloadDTO);
+        String userExpectedResponse = writer.writeValueAsString(responseDTO);
+
+        MvcResult response = mvc.perform(MockMvcRequestBuilders.post("/users/create")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(userEntity))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
+                .andReturn();
+        Assert.assertEquals(userExpectedResponse, response.getResponse().getContentAsString());
+    }
+
+    @Test
+    public void buscarTodosLosUseres() throws Exception {
+        MvcResult response = mvc.perform(MockMvcRequestBuilders.get("/users/all")
                         .accept(MediaType.APPLICATION_JSON))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
@@ -71,8 +78,8 @@ public class ReservationIntegrationTests {
     }
 
     @Test
-    public void buscarReservaPorId() throws Exception {
-        ResultActions response = mvc.perform(MockMvcRequestBuilders.get("/reservations/get/{id}", 2)
+    public void buscarUserPorId() throws Exception {
+        ResultActions response = mvc.perform(MockMvcRequestBuilders.get("/users/get/{id}", 2)
                         .accept(MediaType.APPLICATION_JSON))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isOk());
@@ -80,8 +87,8 @@ public class ReservationIntegrationTests {
     }
 
     @Test
-    public void buscarReservaPorIdNoExistente() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.get("/reservations/get/{id}", 30)
+    public void buscarUserPorIdNoExistente() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/users/get/{id}", 30)
                         .accept(MediaType.APPLICATION_JSON))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isNotFound());
